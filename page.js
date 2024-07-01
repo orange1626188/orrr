858b266f21f1b1d83b5df995e3ff97873168ec8a
unknown <orange>
2024-07-01 10:29:11 +0700

    first commit

---
+++ page.js
@@ -0,0 +1,27 @@
+export default function Home() {
+  return (
+    <>
+    <div className="container ">
+      <div className="row">
+        <div className="col-12 text-center bg-primary p-5">
+                HEADER
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 text-center bg-warning p-5">
+          CONTENT1
+        </div>
+        <div className="col-md-6 text-center bg-success p-5">
+          RCONTENT2
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-md-12 text-center bg-info p-5">
+        FOOTER
+        </div>
+      </div>
+      
+    </div>
+    </>
+  );
+}
